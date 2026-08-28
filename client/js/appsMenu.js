6d336524f5e82ebe817745f7bb401ed5f223c6fa
DohaoSTR <muzalevskij.evgenij@mail.ru>
2026-08-28 14:46:35 +0300

    fix: Пункт в меню приложений больше не прячется из-за права модуля.

--- client/js/appsMenu.js
+++ client/js/appsMenu.js
@@ -6,16 +6,6 @@ import bridge from '@/integrations/ModuleBridge.js'
 import { APPS_MENU_ITEMS_GROUP } from '@/integrations/moduleContracts.js'
 import { tGlobal } from '@/i18n/index.js'
 import { DEFAULT_CHAT_PROFILE_ID } from './chatProfiles.js'
-import { AI_ASSISTANT_MODULE, AI_ASSISTANT_PERMISSIONS } from './permissionKeys.js'
-
-async function checkOllamaChatVisibility() {
-  const { isAiAssistantAclDenied } = await import('./aiAssistantAccess.js')
-  if (await isAiAssistantAclDenied()) {
-    return false
-  }
-  const { hasModulePermission } = await import('@/core/cms/adp/js/accessControl.js')
-  return hasModulePermission(AI_ASSISTANT_MODULE, AI_ASSISTANT_PERMISSIONS.MINI_CHAT)
-}
 
 bridge.provideMany(APPS_MENU_ITEMS_GROUP, 'ai_assistant_ollama_chat', {
   id: 'ai_assistant_ollama_chat',
@@ -27,5 +17,4 @@ bridge.provideMany(APPS_MENU_ITEMS_GROUP, 'ai_assistant_ollama_chat', {
   onClick: () => {
     return bridge.call('ai_assistant.mini_chat.open', DEFAULT_CHAT_PROFILE_ID, { default: false })
   },
-  isVisible: checkOllamaChatVisibility,
 })
